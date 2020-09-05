aa9cf61f4c2778c50d007efd6bcca8ee96b8a047
Rahul Rathod <65287267+rahulmns@users.noreply.github.com>
2020-09-05 16:26:30 +0530

    Initial Commit

---
+++ WeatherRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherRich
+//
+//  Created by Rahul on 9/5/20.
+//  Copyright © 2020 Rahul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
